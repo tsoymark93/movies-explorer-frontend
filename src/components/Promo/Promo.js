ed90cab9ccf1d9e5f4540fae13db9ed4dab0b200
Mark Tsoy <tsoy.mark@MacBook-Air-Mark.local>
2023-08-08 14:00:09 +0600

    добавил скролл на кнопку на главной странице

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -2,19 +2,30 @@ import React from 'react';
 import './Promo.css';
 import logo from '../../images/landing-logo.svg';
 
-const Promo = () => (
-    <section className="promo">
-        <div className="promo__container">
-            <img src={logo} className="promo__logo" alt="Логотип Планета"></img>
-            <div className="promo__info">
-                <h1 className="promo__title">Учебный проект студента факультета Веб&#8209;разработки.</h1>
-                <p className="promo__text">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
-                <a className="link promo__link" href="#about-project">
-                    Узнать больше
-                </a>
+const Promo = () => {
+    const handleScroll = () => {
+        const aboutProjectSection = document.getElementById('about-project');
+        if (aboutProjectSection) {
+            aboutProjectSection.scrollIntoView({
+                behavior: 'smooth',
+            });
+        }
+    };
+
+    return (
+        <section className="promo">
+            <div className="promo__container">
+                <img src={logo} className="promo__logo" alt="Логотип Планета"></img>
+                <div className="promo__info">
+                    <h1 className="promo__title">Учебный проект студента факультета Веб&#8209;разработки.</h1>
+                    <p className="promo__text">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+                    <a className="link promo__link" onClick={handleScroll}>
+                        Узнать больше
+                    </a>
+                </div>
             </div>
-        </div>
-    </section>
-);
+        </section>
+    );
+};
 
 export default Promo;
